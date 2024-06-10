ccb05fc3809358e8337824351ed2ab95f0bb3e5f
D-17 <D-17@D-17>
2024-06-10 20:20:14 +0700

    membuat game tebak angka

---
+++ oop_pertemuan2/src/main/java/com/rwf/oop_pertemuan2/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rwf.oop_pertemuan2;
+
+/**
+ *
+ * @author D-17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Game gamebaru = new Game(3);
+        gamebaru.play();
+    }
+}
